dbbce2fe823894416a9eb71da329c83aec93d303
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-10 22:32:49 +0400

    Create views.py

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Book
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
