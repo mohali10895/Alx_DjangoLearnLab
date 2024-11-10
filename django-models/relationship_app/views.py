45fca0cc3d925b8059b9e8de9bec34d07eb29d20
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-10 23:17:11 +0400

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,3 +14,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# Custom registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('list_books')  # Redirect to a relevant page
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
