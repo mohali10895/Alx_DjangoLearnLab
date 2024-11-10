73544f545aa5316998834fda462e7a9485af0a25
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-10 22:57:16 +0400

    Update query_samples.py

--- django-models/query_samples.py
+++ django-models/query_samples.py
@@ -14,16 +14,3 @@ def books_in_library(library_name):
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
-from django.views.generic.detail import DetailView  # Correct import for DetailView
-from .models import Book, Library
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# Class-based view to display library details and books in it
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
