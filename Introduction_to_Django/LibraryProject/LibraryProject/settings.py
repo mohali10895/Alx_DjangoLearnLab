f6a0fa18b214008da2d9f120d60f998e170ad161
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-11 00:37:17 +0400

    Update settings.py

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,10 @@
 
+INSTALLED_APPS = [
+    'django.contrib.admin',  # Django admin interface
+    'django.contrib.auth',   # Authentication system
+    'django.contrib.contenttypes',  # Content types framework
+    'django.contrib.sessions',  # Session management
+    'django.contrib.messages',  # Messaging framework
+    'django.contrib.staticfiles',  # Static files management
+    'bookshelf',  # Your app (e.g., 'bookshelf')
+]
