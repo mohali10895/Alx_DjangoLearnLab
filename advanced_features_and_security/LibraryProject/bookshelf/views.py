1185bfea6c1d6451cccbf1457c7d17ac179b7177
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-17 08:50:51 +0400

    Create views.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import UpdateView, CreateView
+from .models import Book
+
+# Function-based views
+@permission_required('app_name.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'books/view_books.html', {'books': books})
+
+@permission_required('app_name.can_create', raise_exception=True)
+def create_book(request):
+    # Logic for creating a book
+    return render(request, 'books/create_book.html')
+
+# Class-based views
+class BookUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Book
+    fields = ['title', 'author', 'published_date']
+    template_name = 'books/edit_book.html'
+    permission_required = 'app_name.can_edit'
