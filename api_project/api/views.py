81d4d71b40c6eae6c42c1da9134d9af133e942d9
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-23 16:35:01 +0400

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import generics.ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
