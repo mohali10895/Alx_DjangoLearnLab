ce4142c386f869c797b51d13d9eda6067b278846
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-23 16:51:08 +0400

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -27,7 +27,7 @@ from rest_framework.viewsets.ModelViewSet import viewsets.ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     """
     A ViewSet for performing CRUD operations on the Book model.
     """
