db8124f696c983fd71dd09f09fcee8e5871b3851
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-23 16:38:40 +0400

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,14 @@ from rest_framework.generics import ListAPIView
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on the Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
