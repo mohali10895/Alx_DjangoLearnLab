cde91b9891ed9a7755cfaa3baf7a2cf3197c7032
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-23 16:56:15 +0400

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,28 +1,3 @@
-from django.urls import path
-from .views import BookList
-
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
-
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-from .views import BookList, BookViewSet
-
-# Initialize the router
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
-
-# Define URL patterns
-urlpatterns = [
-    # Existing route for BookList
-    path('books/', BookList.as_view(), name='book-list'),
-
-    # Include the router's URLs
-    path('', include(router.urls)),
-]
-
-
 from rest_framework.viewsets.ModelViewSet import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
