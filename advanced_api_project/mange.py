df75db1c03bd6c68d89ab36294e09f08f6b123b7
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-11-30 14:37:41 +0400

    Update mange.py

--- advanced_api_project/mange.py
+++ advanced_api_project/mange.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
 
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_api_project.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # If the Django package is not found, it's likely because it's not installed
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and available "
+            "on your PYTHONPATH environment variable? Did you forget to activate a virtual environment?"
+        )
+
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
