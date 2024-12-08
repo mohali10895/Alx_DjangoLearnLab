315e67a0b12cf40d55428446bd4f3030c7a44e1d
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-08 22:05:28 +0400

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,13 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Enter your comment here...'}),
+        }
