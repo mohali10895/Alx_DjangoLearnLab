cd7d708822f99b1a44ba22426fb627be63cb6021
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-08 21:48:14 +0400

    Create settings.py

---
+++ django_blog/django_blog/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+    ...
+    'blog',
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        ...
+        'DIRS': [BASE_DIR / "blog/templates"],
+        ...
+    },
+]
