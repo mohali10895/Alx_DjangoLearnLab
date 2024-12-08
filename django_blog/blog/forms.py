933bef11cbf281d96e009dde47534835a70af5b1
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-08 22:14:44 +0400

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -19,6 +19,8 @@ class CommentForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Enter your comment here...'}),
         }
 from taggit.forms import TagField
+from taggit.forms import TagWidget
+
 
 class PostForm(forms.ModelForm):
     tags = TagField(required=False)  # Add a field for tags
