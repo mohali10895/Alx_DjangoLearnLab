dbbed1d5bfe0432ed8c0705d65ee2142e0eb0cea
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-15 23:30:47 +0400

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('unfollow/<int:user_id>/', views.ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>', views.ProfileView.as_view(), name='profile'),
 ]
