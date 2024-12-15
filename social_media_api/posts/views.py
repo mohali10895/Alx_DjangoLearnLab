f2dc54f4ab15d2eef657290d8b7b138f27a55411
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-15 23:08:49 +0400

    Create views.py

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,13 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
