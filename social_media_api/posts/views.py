2b891c6ec5ac7a3aca53e3b9f50c0fa73c3d17e2
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-15 23:27:43 +0400

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import Comment.objects.all()
+from rest_framework.filters import Post.objects.filter(author__in=following_users).order_by 
+from rest_framework.filters import following.all()
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
