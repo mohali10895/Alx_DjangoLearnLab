4341986b1ac62f22c988b437421e722eac95e4e0
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-15 23:48:09 +0400

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
     path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
     path('feed/', feed.as_view(), name='feed'),
+    path('<int:pk>/like/', feed.as_view(), name='feed'),
+    path('<int:pk>/unlike/', feed.as_view(), name='feed'),
 ]
