075687c992bdb4967971fa51c51943d5c57bb13e
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-12-16 00:08:37 +0400

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -14,3 +14,10 @@ SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
 DATABASES = {
     'default': dj_database_url.config(default='postgres://USER:PASSWORD@HOST:PORT/NAME')
 }
+import os
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
